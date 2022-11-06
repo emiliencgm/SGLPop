e2cca9cfd0d97071f262685afa80fa68c2b52de7
emiliencgm <64917214+emiliencgm@users.noreply.github.com>
2022-11-06 11:04:25 +0800

    Add files via upload

---
+++ code/run-MIND.py
@@ -0,0 +1,13 @@
+import os
+cuda = 0
+os.system('python main.py --if_big_matrix 1 --n_fold 2 --edge_drop_prob 0.1 --dataset MIND --if_pop 0 --cuda {} --comment SGL_Origin'.format(cuda))
+os.system('python main.py --if_big_matrix 1 --n_fold 2 --edge_drop_prob 0.1 --dataset MIND --if_pop 1 --cuda {} --comment SGL_Adaptive'.format(cuda))
+os.system('python main.py --if_big_matrix 1 --n_fold 2 --edge_drop_prob 0.0 --dataset MIND --if_pop 0 --cuda {} --lambda1 0. --comment LightGCN'.format(cuda))
+os.system('python main.py --if_big_matrix 1 --n_fold 2 --edge_drop_prob 0.0 --dataset MIND --if_pop 0 --cuda {} --comment LGN+InfoNCE'.format(cuda))
+
+#调整drop和add的概率超参数
+for drop in [0.05, 0.1, 0.2, 0.3]:
+    for add in [0.8, 0.9, 1.0]:
+        P_e_drop1, P_e_drop2 = drop, drop
+        P_e_add1, P_e_add2 = add, add
+        os.system('python main.py --if_big_matrix 1 --n_fold 2 --edge_drop_prob 0.1 --dataset MIND --if_pop 1 --cuda {} --P_e_drop1 {} --P_e_add1 {} --P_e_drop2 {} --P_e_add2 {} --comment SGL_Adaptive_HyperParamSearch'.format(cuda, P_e_drop1, P_e_add1, P_e_drop2, P_e_add2))
